10b9b4a951664c17c1ed1b083107268ddcbb3c97
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-03-17 16:43:22 -0700

    Updating check-in parameter

--- sd_mongo_client.py
+++ sd_mongo_client.py
@@ -12,7 +12,7 @@ result = subprocess.check_output("mongostat -n 1", shell=True)
 active_connections = int(result.split()[-2]) - 1 # subtract one for this connection
 
 # POST the active connections to the spin-docker client
-req = Request('http://%s/v1/check-in' % docker_ip, data='active-connections=%s' % active_connections)
+req = Request('http://%s/v1/check-in' % docker_ip, data='active=%s' % active_connections)
 try:
     resp = urlopen(req)
 except Exception:
